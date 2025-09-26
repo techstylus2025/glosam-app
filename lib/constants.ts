46c354e288e732fe78030b1fd29f567d03a74967
Gyei-Sam <sagyei140@gmail.com>
2025-09-26 08:29:45 +0000

    Initial commit

---
+++ lib/constants.ts
@@ -0,0 +1,6 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'GloSam'
+export const APP_SLOGAN =
+  process.env.NEXT_PUBLIC_APP_SLOGAN || 'Spend less, Look elegant'
+export const APP_DESCRIPTION =
+  process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
+  "GloSam is your ultimate fashion companion, helping you discover the best deals on clothing and accessories from top online retailers. With GloSam, you can effortlessly compare prices, find exclusive discounts, and stay updated on the latest fashion trends. Whether you're shopping for a new wardrobe or just looking for a great deal, GloSam makes it easy to look stylish without breaking the bank."
