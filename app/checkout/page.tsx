b326a8aa579b0a439b66ac5bbd0ee7cb4950fa16
techstylus <techstylus.2025@gmail.com>
2025-10-03 13:21:26 +0000

    17-create-checkout-page

--- app/checkout/page.tsx
+++ app/checkout/page.tsx
@@ -1,4 +1,5 @@
 import { Metadata } from 'next'
+import CheckoutForm from './checkout-form'
 import { auth } from '@/auth'
 import { redirect } from 'next/navigation'
 
@@ -11,5 +12,5 @@ export default async function CheckoutPage() {
   if (!session?.user) {
     redirect('/sign-in?callbackUrl=/checkout')
   }
-  return <div>Checkout Form</div>
+  return <CheckoutForm />
 }
