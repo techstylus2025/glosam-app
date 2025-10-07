3a9091f33461f9276b04bbd2f0a64013d9b6867b
techstylus <techstylus.2025@gmail.com>
2025-10-07 09:32:08 +0000

    21-rate-review-products

--- types/index.ts
+++ types/index.ts
@@ -7,14 +7,29 @@ import {
   UserSignInSchema,
   OrderInputSchema,
   UserSignUpSchema,
+  ReviewInputSchema,
 } from '@/lib/validator'
 import { z } from 'zod'
+
+export type IReviewInput = z.infer<typeof ReviewInputSchema>
+export type IReviewDetails = IReviewInput & {
+  _id: string
+  createdAt: string
+  user: {
+    name: string
+  }
+}
 
 export type IProductInput = z.infer<typeof ProductInputSchema>
 
 export type Data = {
   users: IUserInput[]
   products: IProductInput[]
+  reviews: {
+    title: string
+    rating: number
+    comment: string
+  }[]
   headerMenus: {
     name: string
     href: string
